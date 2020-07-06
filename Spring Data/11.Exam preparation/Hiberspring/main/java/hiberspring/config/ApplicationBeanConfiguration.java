c04bb1241084732eec57d4df6582402b3531c934
Evuns <47922393+Evuns@users.noreply.github.com>
2020-07-06 23:36:25 +0300

    Add files via upload

---
+++ Spring Data/11.Exam preparation/Hiberspring/main/java/hiberspring/config/ApplicationBeanConfiguration.java
@@ -0,0 +1,30 @@
+package hiberspring.config;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import hiberspring.util.*;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationBeanConfiguration {
+
+
+    @Bean
+    public Gson gson() {
+        return new GsonBuilder()
+                .setPrettyPrinting()
+                .create();
+    }
+
+    @Bean
+    public ValidationUtil validationUtil() {
+        return new ValidationUtilImpl();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
